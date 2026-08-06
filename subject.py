601bc635a379633bf4fca0f75af9d02c03c7f11b
alveera mujawar <alveera.mujawar25@mmit.edu.in>
2026-08-06 09:50:39 +0530

    file subject.py is added in the repo

---
+++ subject.py
@@ -0,0 +1,20 @@
+print("Choose the subject you like from the following: maths,physics,programming,biology,chemistry,circuits")
+
+sub1=input("Enter the subject you like:")
+sub2=input("Enter the 2nd subject you like:")
+
+if sub1=="maths" and sub2=="physics":
+  print("Suggest career path: Mechanical Engineering")
+
+elif sub1=="programming" and sub2=="maths":
+  print("Suggest career path: computer engineering")
+
+elif sub1=="biology" and sub2=="chemistry":
+  print("Suggest career path: Biotechnology")
+
+elif sub1=="circuits" and sub2=="maths":
+  print("Suggest career path: Electronic Engineering")
+ 
+else:
+  print("Suggest career path: Explore subjects")
+
