2c91723eac2a775fe394debab73a09545307d100
tanvishelar26 <tanvi.shelar25@mmit.edu.in>
2026-09-03 09:45:07 +0530

    Update subject2.py

--- subject2.py
+++ subject2.py
@@ -11,27 +11,52 @@ class CareerExpertSystem(KnowledgeEngine):
     @Rule(StudentFacts(likes='Graphics'), StudentFacts(likes='Maths'))
     def civil(self):
         print("Suggested Career Path: Civil Engineering")
-    @Rule(StudentFacts(likes='Electronics'), StudentFacts(likes='Maths'), StudentFacts(likes='Programming'))
+    @Rule(StudentFacts(likes='Electronics'), StudentFacts(likes='Maths'))
     def mechatronics(self):
         print("Suggested Career Path: Mechatronics Engineering")
     @Rule(StudentFacts(likes='Programming'), StudentFacts(likes='Artificial Intelligence'))
     def aids(self):
         print("Suggested Career Path: Artificial Intelligence and Data Science")
-    @Rule(StudentFacts(likes='Programming'), StudentFacts(likes='Artificial Intelligence'), StudentFacts(likes='Maths'))
-    def roai(self):
-        print("Suggested Career Path: Robotics and Artificial Intelligence")
+    @Rule(StudentFacts(likes='Programming'), StudentFacts(likes='Robotics'))
+    def robotics(self):
+        print("Suggested Career Path: Robotics Engineering")
+    @Rule(StudentFacts(likes='Biology'), StudentFacts(likes='Chemistry'))
+    def biotechnology(self):
+        print("Suggested Career Path: Biotechnology")
+    @Rule(StudentFacts(likes='Chemistry'), StudentFacts(likes='Physics'))
+    def chemical(self):
+        print("Suggested Career Path: Chemical Engineering")
+    @Rule(StudentFacts(likes='Circuits'), StudentFacts(likes='Maths'))
+    def electronics(self):
+        print("Suggested Career Path: Electronics Engineering")
+    @Rule(StudentFacts(likes='Biology'), StudentFacts(likes='Physics'))
+    def biomedical(self):
+        print("Suggested Career Path: Biomedical Engineering")
 def main():
     engine = CareerExpertSystem()
     engine.reset()
     print("Welcome to the Career Path Expert System!")
-    print("Enter any 2 Subjects from the list given:\nMaths\nPhysics\nGraphics\nArtificial Intelligence\nProgramming\n")
-    
-    interests = input("Enter your interests separated by commas (e.g., Maths, Physics, Programming): ").split(',')
-    for interest in interests:
-        engine.declare(StudentFacts(likes=interest.strip()))
+    print("""Enter any 2 subjects from the list given:
+Maths
+Physics
+Graphics
+Artificial Intelligence
+Programming
+Biology
+Chemistry
+Circuits
+Electronics
+Robotics
+""")
+
+    sub1 = input("Enter your 1st subject: ")
+    sub2 = input("Enter your 2nd subject: ")
+
+    engine.declare(StudentFacts(likes=sub1.strip()))
+    engine.declare(StudentFacts(likes=sub2.strip()))
     engine.run()
+
 if __name__ == "__main__":
     main()
-
 
 
